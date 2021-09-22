d79d48b4a3a185144ecd86928075c10f94aa0768
solid03 <90660726+solid03@users.noreply.github.com>
2021-09-22 04:47:17 -0400

    Add files via upload

---
+++ Clocktower/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Write("Enter a number: ");
+string input = Console.ReadLine();
+
+int number = Convert.ToInt32(input);
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Tick");
+}
+else { 
+    Console.WriteLine("Tock");
+}
