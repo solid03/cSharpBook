e198e873ba17d8f4cc2f4b6494ab48917f5f9732
solid03 <90660726+solid03@users.noreply.github.com>
2021-09-22 05:07:00 -0400

    Add files via upload

---
+++ Watchtower/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+Console.Write("Enter X coordinate: ");
+string xString = Console.ReadLine();
+Console.Write("Enter Y coordinate: ");
+string yString = Console.ReadLine();
+
+int xCoordinate = Convert.ToInt32(xString);
+int yCoordinate = Convert.ToInt32(yString);
+
+if (xCoordinate < 0) {
+    if (yCoordinate < 0) {
+        Console.WriteLine("Northwest!");    
+    }
+    if (yCoordinate == 0) {
+        Console.WriteLine("West!");
+    }
+    if (yCoordinate > 0) {
+        Console.WriteLine("Southwest!");
+    }
+}
+if (xCoordinate == 0) {
+    if (yCoordinate < 0)
+    {
+        Console.WriteLine("North!");
+    }
+    if (yCoordinate == 0)
+    {
+        Console.WriteLine("The Enemy is here!");
+    }
+    if (yCoordinate > 0)
+    {
+        Console.WriteLine("South!");
+    }
+}
+if (xCoordinate > 0)
+{
+    if (yCoordinate > 0)
+    {
+        Console.WriteLine("Northeast!");
+    }
+    if (yCoordinate == 0)
+    {
+        Console.WriteLine("East!");
+    }
+    if (yCoordinate < 0)
+    {
+        Console.WriteLine("Southeast!");
+    }
+}
