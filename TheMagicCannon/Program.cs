cbaa1134bf9d7f12b10a5cf82f2e1ac77d853055
solid03 <90660726+solid03@users.noreply.github.com>
2021-10-07 17:29:08 -0400

    Add files via upload

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+string status = "";
+for (int i = 1; i <= 100; i++) 
+{
+    if ((i % 15) == 0)
+    {
+        status = "Electric and Fire";
+        Console.ForegroundColor = ConsoleColor.Blue;
+    }
+    else if ((i % 5 == 0))
+    {
+        status = "Electric";
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+    else if ((i % 3 == 0))
+    {
+        status = "Fire";
+        Console.ForegroundColor = ConsoleColor.Red;
+    }
+    else
+    {
+        status = "Normal";
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    Console.WriteLine($"{i}: {status}");
+}
