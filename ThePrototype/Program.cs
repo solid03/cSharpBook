2a17564939055498010b39eac74f7e938006aa25
solid03 <90660726+solid03@users.noreply.github.com>
2021-10-07 17:16:04 -0400

    Add files via upload

---
+++ ThePrototype/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+int number = 0;
+int guess = 0;
+
+while (number < 1 || number > 100)
+{
+    Console.Write("User 1:\tPick a number between 1 and 100: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("User 2:\tGuess the number.");
+while (guess != number)
+{
+    Console.Write("What is your guess? ");
+    guess = Convert.ToInt32(Console.ReadLine());
+    if (guess > number)
+    {
+        Console.WriteLine($"{guess} is too high.");
+        continue;
+    }
+    else if (guess < number)
+    {
+        Console.WriteLine($"{guess} is too low.");
+        continue;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("You guessed the number!");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
