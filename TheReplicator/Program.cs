b54a717b835202b01ad81b08bf8e5becfb87c674
solid03 <90660726+solid03@users.noreply.github.com>
2021-10-12 05:04:39 -0400

    Add files via upload

---
+++ TheReplicator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+int[] arrayOne = new int[5];
+int[] arrayTwo = new int[5];
+for (int i = 0; i < arrayOne.Length; i++)
+{
+    Console.Write($"Please enter a number for array index {i}: ");
+    arrayOne[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("arrayOne\tarrayTwo");
+Console.WriteLine("--------\t--------");
+for (int i = 0; i < arrayOne.Length; i++)
+{
+    System.Threading.Thread.Sleep(500);
+    arrayTwo[i] = arrayOne[i];
+    Console.WriteLine($"{arrayOne[i]}\t->\t{arrayTwo[i]}");
+}
