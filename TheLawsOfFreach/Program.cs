98b11712c6747345d85c5a3e1df651830106aaa7
solid03 <90660726+solid03@users.noreply.github.com>
2021-10-12 05:20:01 -0400

    Add files via upload

---
+++ TheLawsOfFreach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentMinimum = int.MaxValue;
+foreach (int currentValue in array) 
+{
+    currentMinimum = Math.Min(currentValue, currentMinimum);
+}
+Console.WriteLine($"Lowest Number:\t{currentMinimum}");
+
+int total = 0;
+foreach (int currentValue in array) 
+{
+    total += currentValue;
+}
+float average = (float)total / array.Length;
+Console.WriteLine($"Average Number:\t{average}");
